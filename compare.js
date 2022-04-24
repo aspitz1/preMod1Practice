8f2841ef18a416bb1c6d7d6b531558fd46bc01b7
Anna Spitz <aspitzemail@gmail.com>
2022-04-24 15:56:29 -0500

    Update annotation

--- compare.js
+++ compare.js
@@ -8,6 +8,9 @@ markCoffeeScore = markBeans / markCoffee ** 2;
 johnCoffeeScore = johnBeans / johnCoffee ** 2;
 markHigherScore = markCoffeeScore > johnCoffeeScore;
 // who had the higher socre?
+// expected output 26.9
 console.log(Math.round(markCoffeeScore * 10) / 10);
+// expected output 27.4
 console.log(Math.round(johnCoffeeScore * 10) / 10);
+// expected output false
 console.log(markHigherScore);
